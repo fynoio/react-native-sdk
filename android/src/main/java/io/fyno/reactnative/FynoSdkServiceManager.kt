cf623ea7d59103f7a68bcd1129735411ab5dc521
Viram Jain <viram@fyno.io>
2024-11-26 13:33:19 +0530

    added notification emitters

--- android/src/main/java/io/fyno/reactnative/FynoSdkServiceManager.kt
+++ android/src/main/java/io/fyno/reactnative/FynoSdkServiceManager.kt
@@ -1,19 +1,58 @@
 package io.fyno.reactnative
 
-import com.facebook.react.bridge.Callback
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import android.content.Context.RECEIVER_EXPORTED
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.util.Log
+import com.facebook.react.bridge.LifecycleEventListener
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.modules.core.DeviceEventManagerModule
 import io.fyno.callback.models.MessageStatus
 import io.fyno.kotlin_sdk.FynoSdk
+import io.fyno.pushlibrary.FynoCallbacks
 import io.fyno.pushlibrary.FynoPush
-import io.fyno.core.FynoUser
+import io.fyno.pushlibrary.helper.NotificationHelper.rawMessage
 import io.fyno.pushlibrary.models.PushRegion
 import org.json.JSONObject
 
 class FynoSdkServiceManager(reactContext: ReactApplicationContext):ReactContextBaseJavaModule(reactContext) {
     override fun getName(): String {
         return "FynoReactNative";
+    }
+
+    init {
+        val filter = IntentFilter()
+        filter.addAction("io.fyno.pushlibrary.NOTIFICATION_ACTION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            reactApplicationContext.registerReceiver(NotificationCallbacks(), filter,  RECEIVER_EXPORTED)
+        }
+    }
+
+    override fun initialize() {
+        super.initialize()
+        handleInitialIntent()
+    }
+
+    private fun handleInitialIntent() {
+        val activity = currentActivity ?: return
+        val intent = activity.intent
+        handleNotificationIntent(intent)
+    }
+
+    private fun handleNotificationIntent(intent: Intent?) {
+        val notificationPayload = intent?.getStringExtra("io.fyno.pushlibrary.notification.payload")
+        if (notificationPayload != null) {
+            sendEvent("onNotificationClicked", notificationPayload)
+        }
+    }
+
+    private fun sendEvent(eventName: String, params: String) {
+        reactApplicationContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            .emit(eventName, params)
     }
 
     @ReactMethod(isBlockingSynchronousMethod = true)
